8455779fce27377313f4988c3f0eef5c65837578
LeoLuxo <leo@gamboa.eu>
2021-04-06 14:45:57 +0200

    Remove old icons

--- app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/selectgrid/SelectLocalImportModel.java
+++ app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/selectgrid/SelectLocalImportModel.java
@@ -51,10 +51,10 @@ public class SelectLocalImportModel {
 	
 	public static Drawable[] getimage(Context mcontext) {
 		Drawable[] title_Array = new Drawable[]{
-				ContextCompat.getDrawable(mcontext, R.drawable.conimport),
-				ContextCompat.getDrawable(mcontext, R.drawable.conloasl),
-				ContextCompat.getDrawable(mcontext, R.drawable.bioimport),
-				ContextCompat.getDrawable(mcontext, R.drawable.bioimport)
+				ContextCompat.getDrawable(mcontext, R.drawable.origin_importedconventional),
+				ContextCompat.getDrawable(mcontext, R.drawable.origin_importedorganic),
+				ContextCompat.getDrawable(mcontext, R.drawable.origin_localconventional),
+				ContextCompat.getDrawable(mcontext, R.drawable.origin_localorganic)
 		};
 		return title_Array;
 	}
@@ -68,10 +68,10 @@ public class SelectLocalImportModel {
 	
 	public static Drawable[] getIndicatorDrawable(Context mcontext) {
 		Drawable[] titleArray = new Drawable[]{
-				ContextCompat.getDrawable(mcontext, R.drawable.socio_economique_icon),
-				ContextCompat.getDrawable(mcontext, R.drawable.socio_ecologique_icon),
-				ContextCompat.getDrawable(mcontext, R.drawable.socio_culturel_icon),
-				ContextCompat.getDrawable(mcontext, R.drawable.socio_politique_icon)
+				ContextCompat.getDrawable(mcontext, R.drawable.indicator_cat_socialwellbeing),
+				ContextCompat.getDrawable(mcontext, R.drawable.indicator_cat_environment),
+				ContextCompat.getDrawable(mcontext, R.drawable.indicator_cat_economicwellbeing),
+				ContextCompat.getDrawable(mcontext, R.drawable.indicator_cat_goodgovernance)
 		};
 //        return titleArray;
 		return null;
