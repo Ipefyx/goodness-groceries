f995e3798b7f0bafd93920a99c3e1f85cd0431e0
LeoLuxo <leo@gamboa.eu>
2021-05-03 10:11:48 +0200

    Fix of "Onbording" pages first part

--- app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/onbord/OnbordingActivity.java
+++ app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/onbord/OnbordingActivity.java
@@ -5,35 +5,20 @@ import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.PopupMenu;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.bumptech.glide.util.Util;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
-
 import org.json.JSONObject;
-
-import java.lang.reflect.Type;
-import java.util.List;
 
 import lu.uni.bicslab.greenbot.android.MainActivity;
 import lu.uni.bicslab.greenbot.android.R;
 import lu.uni.bicslab.greenbot.android.other.Profile;
-import lu.uni.bicslab.greenbot.android.ui.activity.feedback.FeedbackMainActivity;
-import lu.uni.bicslab.greenbot.android.ui.activity.feedback.ProductToReview;
 import lu.uni.bicslab.greenbot.android.ui.activity.scan.SigninActivity;
 import lu.uni.bicslab.greenbot.android.databinding.OnbordingMainLayoutBinding;
 import lu.uni.bicslab.greenbot.android.barcoderead.transformers.Utils;
@@ -47,7 +32,7 @@ public class OnbordingActivity extends AppCompatActivity {
 	// private ViewsSliderAdapter mAdapter;
 	private ViewsSliderAdapter mAdapter;
 	private TextView[] dots;
-	private int[] layouts;
+	private int[] pages;
 	private OnbordingMainLayoutBinding binding;
 	Profile profile = null;
 	JSONObject jsonObject;
@@ -79,46 +64,30 @@ public class OnbordingActivity extends AppCompatActivity {
 	private void init() {
 		// layouts of all welcome sliders
 		// add few more layouts if you want
-		layouts = new int[]{
+		pages = new int[] {
 				R.layout.onbording_two_layout,
 				R.layout.onbording_one_layout,
 				R.layout.onbording_three_layout};
 		
 		mAdapter = new ViewsSliderAdapter();
 		binding.viewPager.setAdapter(mAdapter);
-		binding.viewPager.registerOnPageChangeCallback(pageChangeCallback);
-		binding.btnSkip.setOnClickListener(new View.OnClickListener() {
+		binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
 			@Override
-			public void onClick(View v) {
-				finish();
-				launchHomeScreen(0);
-				
+			public void onPageSelected(int position) {
+				super.onPageSelected(position);
+				updateBottomDots(position);
 			}
 		});
 		
-		binding.btnNext.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// checking for last page
-				// if last page home screen will be launched
-				int current = getItem(+1);
-				if (current <= layouts.length) {
-					// move to next screen
-					//if(initLogin) {
-					//Toast.makeText(getApplicationContext(), "slides_ended"+current, Toast.LENGTH_LONG).show();
-					launchHomeScreen(current);
-					//}else {
-					//    binding.viewPager.setCurrentItem(current);
-					// }
-				}
-//                else {
-//                    launchHomeScreen();
-//                }
-				
+		binding.btnNext.setOnClickListener(v -> {
+			int current = binding.viewPager.getCurrentItem();
+			if (current < pages.length) {
+				binding.viewPager.setCurrentItem(current+1);
 			}
 		});
+		
 		// adding bottom dots
-		addBottomDots(0);
+		updateBottomDots(0);
 		
 		if (profile.isLogedin() == lu.uni.bicslab.greenbot.android.other.Utils.user_notloggedin) {
 			binding.viewPager.setPageTransformer(Utils.getTransformer(0));
@@ -130,16 +99,10 @@ public class OnbordingActivity extends AppCompatActivity {
 			binding.viewPager.getAdapter().notifyDataSetChanged();
 		}//scroll avoid
 		if (profile.isLogedin() == lu.uni.bicslab.greenbot.android.other.Utils.user_notloggedin) {
-			binding.btnSkip.setVisibility(View.GONE);
 			binding.viewPager.setUserInputEnabled(false);
 		} else {
-			binding.btnSkip.setVisibility(View.VISIBLE);
 			binding.viewPager.setUserInputEnabled(true);
 		}
-	}
-	
-	private int getItem(int i) {
-		return binding.viewPager.getCurrentItem() + i;
 	}
 	
 	private void launchHomeScreen(int count) {
@@ -169,48 +132,27 @@ public class OnbordingActivity extends AppCompatActivity {
 		
 	}
 	
-	/*
-	 * ViewPager page change listener
-	 */
-	ViewPager2.OnPageChangeCallback pageChangeCallback = new ViewPager2.OnPageChangeCallback() {
-		@Override
-		public void onPageSelected(int position) {
-			super.onPageSelected(position);
-			addBottomDots(position);
-			
-			// changing the next button text 'NEXT' / 'GOT IT'
-			if (position == 0) {
-				// last page. make button text to GOT IT
-				binding.btnNext.setText("start");
-				binding.btnSkip.setVisibility(View.GONE);
-			} else {
-				// still pages are left
-				binding.btnNext.setText("next");
-				binding.btnSkip.setVisibility(View.VISIBLE);
-			}
-		}
-	};
 	
 	/*
 	 * Adds bottom dots indicator
 	 * */
-	private void addBottomDots(int currentPage) {
-		dots = new TextView[layouts.length];
+	private void updateBottomDots(int currentPage) {
+		dots = new TextView[pages.length];
 		
-		int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-		int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+		int colorActive = getResources().getColor(R.color.onbord_dot_active, null);
+		int colorInactive = getResources().getColor(R.color.onbord_dot_inactive, null);
 		
 		binding.layoutDots.removeAllViews();
 		for (int i = 0; i < dots.length; i++) {
 			dots[i] = new TextView(this);
 			dots[i].setText(Html.fromHtml("&#8226;"));
-			dots[i].setTextSize(35);
-			dots[i].setTextColor(colorsInactive[currentPage]);
+			dots[i].setTextSize(50);
+			dots[i].setTextColor(colorInactive);
 			binding.layoutDots.addView(dots[i]);
 		}
 		
 		if (dots.length > 0)
-			dots[currentPage].setTextColor(colorsActive[currentPage]);
+			dots[currentPage].setTextColor(colorActive);
 	}
 	
 	
@@ -233,12 +175,12 @@ public class OnbordingActivity extends AppCompatActivity {
 		
 		@Override
 		public int getItemViewType(int position) {
-			return layouts[position];
+			return pages[position];
 		}
 		
 		@Override
 		public int getItemCount() {
-			return layouts.length;
+			return pages.length;
 		}
 		
 		public class SliderViewHolder extends RecyclerView.ViewHolder {
