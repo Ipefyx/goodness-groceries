4be099da9356084424bff1d4ab8a428251dbb0e1
LeoLuxo <leo@gamboa.eu>
2021-07-16 15:10:45 +0200

    Refactor of ProductDetails layout (part 1)

--- app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/itemmain/ProductDetailsActivity.java
+++ app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/itemmain/ProductDetailsActivity.java
@@ -1,68 +1,50 @@
 package lu.uni.bicslab.greenbot.android.ui.activity.itemmain;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.palette.graphics.Palette;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import lu.uni.bicslab.greenbot.android.R;
-import lu.uni.bicslab.greenbot.android.other.CustomAdapter;
 import lu.uni.bicslab.greenbot.android.other.Utils;
 import lu.uni.bicslab.greenbot.android.ui.fragment.compare.CompareActivity;
-import lu.uni.bicslab.greenbot.android.datamodel.IndicatorModel;
 import lu.uni.bicslab.greenbot.android.datamodel.ProductModel;
 import lu.uni.bicslab.greenbot.android.ui.fragment.product_category.ProductCategoryModel;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 	
-	private RecyclerView recyclerView;
-	
 	String productCode;
-	TextView type_data, description, type_category, type_provider;
+	TextView title, description, type_data, provider_data, category_data;
+	ImageView product_image, category_icon;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_productdetails);
-		recyclerView = findViewById(R.id.scrollableview);
-		type_data = findViewById(R.id.type_data);
-		description = findViewById(R.id.description);
-		type_category = findViewById(R.id.type_category);
-		type_provider = findViewById(R.id.type_provider);
 		
-		//  Use when your list size is constant for better performance
-		recyclerView.setHasFixedSize(true);
+		title = findViewById(R.id.title);
+		description = findViewById(R.id.description);
+		type_data = findViewById(R.id.type_data);
+		provider_data = findViewById(R.id.provider_data);
+		category_data = findViewById(R.id.category_data);
+		
+		product_image = findViewById(R.id.product_image);
+		category_icon = findViewById(R.id.category_icon);
 		
 		productCode = getIntent().getExtras().getString("code");
 		
 		Toolbar toolbar = findViewById(R.id.anim_toolbar);
 		setSupportActionBar(toolbar);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-		
+		getSupportActionBar().setTitle(R.string.product);
 		
 		setView(Utils.getProductByCode(this, productCode));
 	}
@@ -96,17 +78,19 @@ public class ProductDetailsActivity extends AppCompatActivity {
 	private void setView(ProductModel product) {
 		ProductCategoryModel category = Utils.getProductCategoryByID(this, product.getCategory());
 		
-		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-		recyclerView.setLayoutManager(linearLayoutManager);
+//		LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+//		recyclerView.setLayoutManager(linearLayoutManager);
+//		
+//		CustomAdapter adapter = new CustomAdapter(this, product.indicators.stream().filter(ind -> ind.isApplicable() && ind.sub_indicators.size() > 0).collect(Collectors.toList()));
+//		recyclerView.setAdapter(adapter);
 		
-		CustomAdapter adapter = new CustomAdapter(this, product.indicators.stream().filter(ind -> ind.isApplicable() && ind.sub_indicators.size() > 0).collect(Collectors.toList()));
-		recyclerView.setAdapter(adapter);
-		
-		getSupportActionBar().setTitle(product.getName());
-		
-		type_data.setText(product.getType());
+		title.setText(product.getName());
 		description.setText(product.getDescription());
-		type_category.setText(category.getName());
-		type_provider.setText(product.getProvider());
+		type_data.setText(product.getType());
+		provider_data.setText(product.getProvider());
+		category_data.setText(category.getName());
+		
+		Glide.with(this).load(Utils.getDrawableImage(this, product.getImage_url())).error(R.drawable.ic_menu_gallery).into(product_image);
+		Glide.with(this).load(Utils.getDrawableImage(this, category.getIcon_name())).error(R.drawable.ic_menu_gallery).into(category_icon);
 	}
 }
