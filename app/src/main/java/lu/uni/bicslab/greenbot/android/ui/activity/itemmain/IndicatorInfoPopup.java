e8f37b2be46038de0de0806407698b6c10cd7aa6
Ipefyx <ipefyx@foxwork.be>
2022-03-10 10:57:21 +0100

    + indicators info popup : add indicator icon

--- app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/itemmain/IndicatorInfoPopup.java
+++ app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/itemmain/IndicatorInfoPopup.java
@@ -5,12 +5,14 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Scroller;
@@ -18,7 +20,9 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.request.RequestOptions;
 
 import org.w3c.dom.Text;
 
@@ -28,11 +32,13 @@ import java.util.List;
 import lu.uni.bicslab.greenbot.android.R;
 import lu.uni.bicslab.greenbot.android.datamodel.IndicatorModel;
 import lu.uni.bicslab.greenbot.android.datamodel.SubIndicatorModel;
+import lu.uni.bicslab.greenbot.android.other.Utils;
 
 public class IndicatorInfoPopup extends Dialog {
 
     private Context mContext;
     private IndicatorModel indicator;
+    private ImageView indicatorIconView;
     private TextView indicatorNameView;
     private LinearLayout descriptionLayout;
     private Button okBtn;
@@ -45,6 +51,7 @@ public class IndicatorInfoPopup extends Dialog {
         this.mContext = context;
         this.indicator = indicator;
 
+        this.indicatorIconView = findViewById(R.id.indicatorIconImage);
         this.indicatorNameView = findViewById(R.id.indicatorNameText);
         this.descriptionLayout = findViewById(R.id.descriptionsLayout);
         this.okBtn = findViewById(R.id.popupIndOkBtn);
@@ -59,6 +66,8 @@ public class IndicatorInfoPopup extends Dialog {
 
     public void build() {
 
+        Drawable img = Utils.getDrawableImage(mContext, indicator.getIcon_name());
+        Glide.with(mContext).load(img).apply(RequestOptions.centerInsideTransform()).into(indicatorIconView);
         indicatorNameView.setText(indicator.getName());
 
         for(SubIndicatorModel sub : indicator.sub_indicators) {
@@ -68,7 +77,7 @@ public class IndicatorInfoPopup extends Dialog {
 
             LinearLayout.LayoutParams txtParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
-            txtParams.setMargins(0,2,0,2);
+            txtParams.setMargins(0,2,0,4);
 
             subName.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
             subName.setLayoutParams(txtParams);
