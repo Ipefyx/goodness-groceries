0d38dcf20e9d48f1b9645f6362b69b5259af0111
Ipefyx <ipefyx@foxwork.be>
2022-04-07 22:05:57 +0200

    * Sign-in id field keep focus hack

--- app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/welcome/SignInFragment.java
+++ app/src/main/java/lu/uni/bicslab/greenbot/android/ui/activity/welcome/SignInFragment.java
@@ -3,6 +3,7 @@ package lu.uni.bicslab.greenbot.android.ui.activity.welcome;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -28,7 +29,7 @@ import lu.uni.bicslab.greenbot.android.ui.activity.scan.BarcodeScannerActivity;
 public class SignInFragment extends Fragment {
 	
 	public static final int BARCODE_CAPTURE = 1;
-	
+
 	private EditText signin_id;
 	
 	private String id;
@@ -47,20 +48,22 @@ public class SignInFragment extends Fragment {
 	@Override
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
+
 		TextView scan_directly = getView().findViewById(R.id.scan_directly);
 		scan_directly.setOnClickListener(v -> {
 			startActivityForResult(new Intent(getActivity(), BarcodeScannerActivity.class), BARCODE_CAPTURE);
 		});
-		
+
+
+
 		signin_id = getView().findViewById(R.id.signin_id);
 		signin_id.addTextChangedListener(new TextWatcher() {
-			
+
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 				// May not be good design as it keeps this Fragment and the WelcomeActivity tightly coupled, but it simplifies this horrible mess of a welcome screen a lot
 				WelcomeActivity activity = ((WelcomeActivity) getActivity());
-				
+
 				if (s.length() != 13) {
 					signin_id.setError(getResources().getString(R.string.id_digit_alert));
 					activity.setIDValid(false);
@@ -79,12 +82,33 @@ public class SignInFragment extends Fragment {
 			@Override public void afterTextChanged(Editable s) {}
 			@Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 		});
+
+
+		// Hacky code to keep focus on signin edittext when keyboard appear
+		signin_id.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean b) {
+				if (b) {
+					// Request focus in a short time because the
+					// keyboard may steal it away.
+					v.postDelayed(new Runnable() {
+						@Override
+						public void run() {
+							if (!v.hasFocus()) {
+								v.requestFocus();
+							}
+						}
+					}, 200);
+				}
+			}
+		});
+
 	}
 	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
+
 		if (requestCode == BARCODE_CAPTURE) {
 			if (data != null) {
 				String barcode = data.getStringExtra("barcode");
